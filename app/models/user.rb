4ca3aa405acca8d2a87e4fa92739dda4df52776e
amon-cofie <ddd.amon123@gmail.com>
2023-02-20 12:46:42 +0000

    Correct all linter errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
-    has_many :categories
-    has_many :expenses
+  has_many :categories
+  has_many :expenses
 end
--- test/controllers/category_expenses_controller_test.rb
+++ test/controllers/category_expenses_controller_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class CategoryExpensesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
