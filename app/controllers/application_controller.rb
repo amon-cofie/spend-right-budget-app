7253a36fa6d65ee88bb121313f7f65e17864e7e3
amon-cofie <ddd.amon123@gmail.com>
2023-02-20 14:01:04 +0000

    Add structure of splash screen

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,14 @@
 class ApplicationController < ActionController::Base
-    before_action :authenticate_user!
-    # before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-    # def after_sign_out_path_for(_scope)
-    #     root_path
-    # end
+  # def after_sign_out_path_for(_scope)
+  #     root_path
+  # end
 
-    # protected
-    # def configure_permitted_parameters
-    #     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email password])
-    # end
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name])
+  end
 end
