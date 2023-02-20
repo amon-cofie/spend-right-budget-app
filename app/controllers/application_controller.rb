a0571d1c2551adabbba32d0c825c6fb5f83f3e9a
amon-cofie <ddd.amon123@gmail.com>
2023-02-20 19:23:28 +0000

    Add categories controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,13 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  # def after_sign_out_path_for(_scope)
-  #     root_path
-  # end
+  def after_sign_out_path_for(_scope)
+    root_path
+  end
+
+  def after_sign_in_path_for(_scope)
+    categories_path
+  end
 
   protected
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :categories do 
+    resources :expenses
+  end
   resources :category_expenses
-  resources :expenses
-  resources :categories
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
